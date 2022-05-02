79e1b47a79f060546e584beef61ea8f41d84bf3f
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-01 21:43:54 -0300

    Componente screen heroe + back from heroe screen

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,56 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../../helpers/getHeroById";
 import "./Hero.style.css";
 
 export const HeroScreen = () => {
-
-  //Hook para recuperar el param :id 
+  //Hook para recuperar el param :id
   const { id } = useParams();
+
+  //Navigate para volver atras
+  const navigate = useNavigate();
+  
+  //Consigo info heroe
   const hero = getHeroById(id);
-  if(!hero){
-   return <Navigate to='/'></Navigate>
+  if (!hero) {
+    return <Navigate to="/"></Navigate>;
   }
 
-  
-  return <div>
-    <h1>Hero Screen</h1>
-    <p>{hero.superhero}</p>
-    </div>;
+  const handleReturn = () => {
+    //Vuelvo atras
+    navigate(-1);
+  };
+
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`/assets/${hero.id}.jpg`}
+          alt={id}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group ">
+          <li className="list-group-item">
+            <b>Alter ego: {hero.alter_ego}</b>
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: {hero.publisher}</b>
+          </li>
+          <li className="list-group-item">
+            <b>First appearence: {hero.first_appearance}</b>
+          </li>
+        </ul>
+        <h5 className="mt-4">Characters</h5>
+        <p>{hero.characters}</p>
+        <button
+          className="btn btn-outline-primary btn-sm"
+          onClick={handleReturn}
+        >
+          Regresar
+        </button>
+      </div>
+    </div>
+  );
 };
