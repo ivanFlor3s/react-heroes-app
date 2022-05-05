1d0705d092da6d22b148eec6b61d51af4a600f62
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-05 00:22:28 -0300

    Rutas publicas login

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -4,16 +4,27 @@ import { LoginScreen } from "../components/login/LoginScreen/LoginScreen";
 import { BrowserRouter } from "react-router-dom";
 import { DashboardRoutes } from "./DashboardRoutes";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { PublicRoute } from "./PublicRoute";
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/*" element={
-          <PrivateRoutes>
-            <DashboardRoutes></DashboardRoutes>
-          </PrivateRoutes>
-        }></Route>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginScreen />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/*"
+          element={
+            <PrivateRoutes>
+              <DashboardRoutes></DashboardRoutes>
+            </PrivateRoutes>
+          }
+        ></Route>
         {/* <Route path="/*" element={<DashboardRoutes/>} /> */}
       </Routes>
     </BrowserRouter>
